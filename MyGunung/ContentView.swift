a79f44fbd1a1436699e88dc54c1e0fe4d5c6cb96
Farrelhasyidan <farrelhasyidan@gmail.com>
2021-04-09 10:12:03 +0700

    Initial Commit

---
+++ MyGunung/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyGunung
+//
+//  Created by Farrel hasyidan on 09/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
